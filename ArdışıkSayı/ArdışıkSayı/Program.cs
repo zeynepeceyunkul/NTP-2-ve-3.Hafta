382924cc7817b6f6539d96d077656cdc5c932f5d
zeynepeceyunkul <eceyunkul44@gmail.com>
2024-10-28 22:18:36 +0300

    ArdışıkSayı

---
+++ ArdışıkSayı/ArdışıkSayı/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArdışıkSayı
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kullanıcıdan tam sayı listesi almak için bir liste tanımlar
+            List<int> sayilar = new List<int>();
+
+            Console.WriteLine("Sayıları girin, çıkmak için 0 girin");
+
+            while (true)
+            {
+                int giris = int.Parse(Console.ReadLine()); // Girişleri int'e çevirir
+
+                if (giris == 0) // Eğer sıfıra eşitse bitirir
+                    break;
+                sayilar.Add(giris); // Değilse listeye ekler
+
+            }
+            sayilar.Sort(); // Sayıları küçükten büyüğe sıralar
+
+            List<String> gruplar = new List<string>(); // Ardışık grupları saklamak için tanımlanan liste
+
+            // Dizide ardışık grupları bulmak için tanımlanan değişkenler
+            int start = sayilar[0];
+            int end = sayilar[0];
+
+            for(int i = 1; i < sayilar.Count; i++)
+            {
+                if(sayilar[i]== end + 1) // eğer sayı ardışık ise end'i günceller
+                {
+                    end = sayilar[i];
+                }
+                else
+                {
+                    // Ardışık sayı grubu bulunduğunda, grubu kaydeder ve yeni bir grup başlatır
+                    if (start == end)
+                        gruplar.Add(start.ToString());
+
+                    else
+                        gruplar.Add(start + "-" + end);
+
+                    // Yeni grubun başlangıcını ve sonunu ayarlar
+                    start = sayilar[i];
+                    end = sayilar[i];
+                    
+                }
+            }
+
+            if (start == end) // Döngü tamamlandığında son grubu da kontrol eder
+                gruplar.Add(start.ToString());
+            else
+                gruplar.Add(start + "-" + end);
+
+            // Ardışık grupları ekrana yazdırır
+            Console.WriteLine("Ardışık sayı grupları:");
+            foreach (var group in gruplar)
+            {
+                Console.WriteLine(group);
+                Console.ReadKey();
+            }
+
+
+        }
+    }
+}
---
+++ ArdışıkSayı/ArdışıkSayı/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("ArdışıkSayı")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ArdışıkSayı")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("c56a9982-17da-4033-ad7f-0accd3cedfe0")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// '*' kullanarak varsayılana ayarlayabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
